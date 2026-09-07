73ba524ab5d024f96848c1d137dc18989900cf7c
Feranmi Oresajo <ferouslos6@gmail.com>
2026-09-07 17:47:30 +0100

    Delete .tmp/slide5-build directory

--- .tmp/slide5-build/help.mjs
+++
@@ -1,8 +0,0 @@
-import { FileBlob, PresentationFile } from '@oai/artifact-tool';
-const source = '/Users/oresajooluwaferanmiidunuoluwa/Desktop/SCHOOL/docs/presentation/HOLibrary-Group4.pptx';
-const deck = await PresentationFile.importPptx(await FileBlob.load(source));
-const slide = deck.slides.getItem(4);
-console.log('slide', Object.getOwnPropertyNames(Object.getPrototypeOf(slide)));
-console.log('shapes', Object.getOwnPropertyNames(Object.getPrototypeOf(slide.shapes)));
-const target = deck.resolve('sh/dgbulwnm');
-console.log('shape', Object.getOwnPropertyNames(Object.getPrototypeOf(target)));
